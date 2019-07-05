bc7a0e30af96ccee9f85cd773feb08d4beae69d9
Chris Desktop <chrisk357@gmail.com>
2019-07-05 01:01:21 -0500

    starting to seed dumy data

---
+++ Models/SeedData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GearShopV2.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            uisng(var context = new GearShopContext(
+                serviceProvider.GetRequiredService<DbContextOptions<GearShopContext>>()))
+            {
+
+            }
+    }
+}
