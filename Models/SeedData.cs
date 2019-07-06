aa584b7fabc04682600cca2c2aa3fc25b7dc520f
Chris Desktop <chrisk357@gmail.com>
2019-07-06 12:31:21 -0500

    Still working on seeding data problems with ILogger

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using GearShopV2.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,17 @@ namespace GearShopV2.Models
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
-            uisng(var context = new GearShopContext(
-                serviceProvider.GetRequiredService<DbContextOptions<GearShopContext>>()))
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService
+                    <DbContextOptions<ApplicationDbContext>>()))
             {
+                //look for jerseys
+                if (context.Jersey.Any())
+                {
+                    return; //Db has been seeded
+                }
 
             }
+        }
     }
 }
