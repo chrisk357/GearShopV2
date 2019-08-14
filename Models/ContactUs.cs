41b15856de7acfbe81d35bb4fb7ee5f5e0bb052f
Chris Desktop <chrisk357@gmail.com>
2019-08-13 23:09:55 -0500

    Fixed Reviews Jersey page now messed up

--- Models/ContactUs.cs
+++ Models/ContactUs.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GearShopV2.Models
 {
     public class ContactUs
     {
-
+        
         public int Id { get; set; }
         [Required, Display(Name = "Name")]
         public string ContactName { get; set; }
