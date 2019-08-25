ba469be2d68eaa924e309869b35a812648c2d304
Chris Desktop <chrisk357@gmail.com>
2019-08-25 14:48:11 -0500

    Started the AngularImplementation

---
+++ wwwroot/ts/storecustomer.js
@@ -0,0 +1,9 @@
+var StoreCustomer = /** @class */ (function () {
+    function StoreCustomer() {
+        this.visits = 0;
+    }
+    return StoreCustomer;
+}());
+var cust = new StoreCustomer();
+cust.visits = 10;
+//# sourceMappingURL=storecustomer.js.map
---
+++ wwwroot/ts/storecustomer.ts
@@ -0,0 +1,8 @@
+﻿class StoreCustomer {
+
+    public visits:number = 0;
+
+}
+
+let cust = new StoreCustomer();
+cust.visits = 10;
